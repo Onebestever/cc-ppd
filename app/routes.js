f1eea0cf1dd9c0ad8f4dbe543579a75556e31bdc
Onebestever <carla.h1248@gmail.com>
2022-06-18 18:17:10 -0400

    feed page + profile + partner need ccs styling + still working on adding todo list with txt message notification reminder

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+
 const user = require("./models/user");
 // require('dotenv').config();
 
@@ -17,30 +18,90 @@ module.exports = function (app, passport, db) {
 
 
 
+
+
+
+
   // PROFILE SECTION =========================
 
-  app.get('/profile', isLoggedIn, async function (req, res) {
-    db.collection('journalEntries').find().toArray((err, result) => {
-      console.log(req.user)
-      if (err) return console.log(err)
-      console.log('this is the result', result.body)
 
+
+
+
+  // PROFILE SECTION =========================
+
+  app.get('/profile', isLoggedIn, function(req, res) {
+    db.collection('journalEntries').find({postedBy: req.user._id}).toArray((err, result) => {
+      // console.log('this is the first',req.user, 
+      // 'this is the specific one', req.user._id, 
+      // 'this is the whole thing', result, 'this is the post',)
+
+      if (err) return // console.log(err)
       res.render('profile.ejs', {
-
+       
         user: req.user,
-        'journalEntries': result,
-        // allUsers: allUsers
-
+        'journalEntries': result
       })
     })
-  });
+});
 
+app.get('/journalEntries/:ObjectId', isLoggedIn, function(req, res) {
+  // console.log('this is objectId',ObjectId)
+  const postId = ObjectId(req.params.ObjectId)
+  // // console.log('this is req params',req.params)
+
+  db.collection('journalEntries').find({_id: postId}).toArray((err, result) => {
+    // // console.log('this is the first',req.user, 
+    // 'this is the specificapp. one', req.user._id, 
+    // 'this is the whole thing', result, 'this is the post',)
+
+    if (err) return // console.log(err)
+    res.render('post.ejs', {
+     
+      user: req.user,
+      'journalEntries': result
+    })
+  })
+});
+
+
+app.get('/page/:id', isLoggedIn, function(req, res) {
+  let postId = ObjectId(req.params.id)
+  db.collection('journalEntries').find({postedBy: postId}).toArray((err, result) => {
+    if (err) return // console.log(err)
+    res.render('page.ejs', {
+      'journalEntries': result
+    })
+  })
+});
+
+
+///////////////POST ACTION/////////////////////////////////
+app.post('/log',  (req, res) => {
+  let user = ObjectId(req.user._id)
+  // console.log(req.body)
+  db.collection('journalEntries').insertOne({
+    dailyPost: req.body.dailyPost,
+    date: req.body.date,
+    emotion: req.body.emotion,
+    stressLevel: req.body.stressLevel,
+    harmful: req.body.harmful,
+    note: req.body.note,
+    starred: false,
+    postedBy: user
+  }, (err, result) => {
+    if (err) return // console.log(err)
+    //// console.log(result)
+    // console.log('saved to database')
+    res.redirect('/profile')
+  })
+})
 
   //VIEWERS LOGIN ////////
   app.get('/viewers', isLoggedIn, function (req, res) {
     db.collection('users').find().toArray((err, result) => {
-      if (err) return console.log(err)
-      console.log(result)
+      if (err) return // console.log(err)
+      // console.log(result)
 
 
       res.render('viewers.ejs', {
@@ -56,12 +117,12 @@ module.exports = function (app, passport, db) {
   //viewer PROFILE
   app.get('/viewers', isLoggedIn, function (req, res) {
     db.collection('journalEntries').find().toArray((err, result) => {
-      if (err) return console.log(err)
-      console.log('result', result)
+      if (err) return // console.log(err)
+      // console.log('result', result)
 
       //update find to filter out/
       // let myWorkLogs = result.filter(doc => doc.name === req.user.local.email)
-      // console.log('myWorkLogs', myWorkLogs)
+      // // console.log('myWorkLogs', myWorkLogs)
 
       res.render('viewers.ejs', {
         user: req.user,
@@ -79,8 +140,8 @@ module.exports = function (app, passport, db) {
   //relationshipGET forclient to get to page //////////////////////////////////////
   app.get('/relationship', isLoggedIn, function (req, res) {
     db.collection('users').find().toArray((err, result) => {
-      if (err) return console.log(err)
-      console.log('result', result)
+      if (err) return // console.log(err)
+      // console.log('result', result)
 
       res.render('relationship.ejs', {
         user: req.user,
@@ -91,32 +152,49 @@ module.exports = function (app, passport, db) {
 
   //relationship routes for client to find and display email
   app.put('/searchAddUser', (req, res) => {
-    console.log('this is the client', req.body.email)
+    console.log('incoming email', req.body.email)
+    console.log('current user email', req.user.local.email)
 //req.user is a mongoos object we can directly push into
 //if  you want to change anything with user target req.user
-    req.user.relationship.push(req.body.email)
-    req.user.save(function(err) {
-         if (err) return console.log(err)
-      console.log('saved to database')
-      res.send({status:'ok'});
-  });
-    // db.collection('users').find({
-    //   'local.email': req.user.local.email
-    // }, {
-    //   $push: {
-    //     'relationship':req.body.email
-    //   } //to add to an array 'relationship'
-    // }, (err, result) => {
-   
-    // })
+  //   req.user.relationship.push(req.body.email)
+  //   req.user.save(function(err) {
+  //        if (err) return // console.log(err)
+  //     // console.log('saved to database')
+  //     res.send({status:'ok'});
+  // });
+    db.collection('users').findOneAndUpdate({
+      'local.email': req.user.local.email
+    }, {
+      $push: {
+        'relationship':req.body.email
+      } //to add to an array 'relationship'
+    }, (err, result) => {
+      if(err){
+        console.log('ERROR',err)
+        return
+      }
+      db.collection('users').findOneAndUpdate({
+        'local.email': req.body.email
+      }, {
+        $push: {
+          'relationship':req.user.local.email
+        } //to add to an array 'relationship'
+      }, (err, result) => {
+        if(err){
+          console.log('ERROR', err)
+          return
+        }
+        return // send respone
+      })
+    })
   })
 
   // partners page Routes ===============================================================
   app.get('/partner', isLoggedIn, async function (req, res) {
     const results = db.collection('journalEntries').find().toArray((err, result) => {
-      console.log(req.user)
-      if (err) return console.log(err)
-      console.log(result)
+      // console.log(req.user)
+      if (err) return // console.log(err)
+      // console.log(result)
 
 
       res.render('partner.ejs', {
@@ -134,9 +212,9 @@ module.exports = function (app, passport, db) {
 
   app.get('/feed', isLoggedIn, async function (req, res) {
     db.collection('journalEntries').find().toArray((err, result) => {
-      console.log(req.user)
-      if (err) return console.log(err)
-      console.log('this is the result', result.body)
+      // console.log(req.user)
+      if (err) return // console.log(err)
+      // console.log('this is the result', result.body)
 
       res.render('feed.ejs', {
 
@@ -154,36 +232,22 @@ module.exports = function (app, passport, db) {
   // viewers Page Routes ===============================================================
 
 
+  // app.post('/makePost', (req, res) => {
+  //   let user = req.user._id
+  //   db.collection('posts').save({caption: req.body.caption, img: 'images/uploads/' + req.file.filename, postedBy: user}, (err, result) => {
+  //     if (err) return // console.log(err)
+  //     // console.log('saved to database')
+  //     res.redirect('/profile')
+  //   })
+  // })
 
-
-
-
-
-
-  app.post('/log', (req, res) => {
-    // console.log(req.body)
-    db.collection('journalEntries').insertOne({
-      dailyPost: req.body.dailyPost,
-      date: req.body.date,
-      emotion: req.body.emotion,
-      stressLevel: req.body.stressLevel,
-      harmful: req.body.harmful,
-      note: req.body.note,
-      starred: false
-    }, (err, result) => {
-      if (err) return console.log(err)
-      //console.log(result)
-      console.log('saved to database')
-      res.redirect('/profile')
-    })
-  })
 
   /* 
 
 
 
   app.put('/favorite', (req, res) => {
-    console.log('hey im the id ' ,req.body)
+    // console.log('hey im the id ' ,req.body)
     db.collection('dailyMoods')
 
     .findOneAndUpdate({
@@ -208,9 +272,9 @@ module.exports = function (app, passport, db) {
 
 
   app.put('/addStar', (req, res) => {
-    console.log(ObjectId(req.body.id))
-    console.log('adding star')
-    console.log('hey im the id ', req.body)
+    // console.log(ObjectId(req.body.id))
+    // console.log('adding star')
+    // console.log('hey im the id ', req.body)
     db.collection('journalEntries')
       .findOneAndUpdate({
         _id: ObjectId(req.body.postObjectID),
@@ -230,7 +294,7 @@ module.exports = function (app, passport, db) {
   })
 
   app.put('/removeStar', (req, res) => {
-    console.log('removing star')
+    // console.log('removing star')
     db.collection('journalEntries')
       .findOneAndUpdate({
         _id: ObjectId(req.body.postObjectID)
@@ -263,8 +327,8 @@ module.exports = function (app, passport, db) {
 
   app.get('/chart', isLoggedIn, function (req, res) {
     db.collection('journalEntries').find().toArray((err, journalEntries) => {
-      if (err) return console.log(err)
-      console.log('result', journalEntries)
+      if (err) return // console.log(err)
+      // console.log('result', journalEntries)
       let stressCount = [0, 0, 0, 0, 0, 0, 0] // sum of stress
       let chartData = [0, 0, 0, 0, 0, 0, 0] // days of the week that take the value of stressLevel
       for (let i = 0; i < journalEntries.length; i++) {
@@ -273,15 +337,15 @@ module.exports = function (app, passport, db) {
         chartData[dayOfWeek] += Number(journalEntries[i].stressLevel)
         stressCount[dayOfWeek]++ //increse
       }
-      console.log(chartData)
+      // console.log(chartData)
       for (let i = 0; i < chartData.length; i++) {
         if (stressCount[i] > 0) {
           chartData[i] = chartData[i] / stressCount[i]
         }
       }
 
-      console.log(chartData)
-      console.log(stressCount)
+      // console.log(chartData)
+      // console.log(stressCount)
 
 
       res.render('chart.ejs', {
@@ -327,11 +391,11 @@ module.exports = function (app, passport, db) {
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }), function (req, res) {
-    console.log('testing signup', req.body)
+    // console.log('testing signup', req.body)
 
     req.user.local.viewerType = req.body.viewerType //set user database to value from ejs
     req.user.save()
-    console.log('thhis is the user ', req.user, req.body.viewerType)
+    // console.log('thhis is the user ', req.user, req.body.viewerType)
     if (req.body.viewerType === 'partner') {
       res.redirect('/partner')
     } else if (req.body.viewerType === 'client') {
